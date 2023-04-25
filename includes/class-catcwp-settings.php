de8ffd9c49e5534fe50fd75070e0690bddc203f2
noruzzamanrubel <noruzzamanrubel@gmail.com>
2023-04-25 18:59:26 +0600

    add more setting

--- includes/class-catcwp-settings.php
+++ includes/class-catcwp-settings.php
@@ -75,26 +75,24 @@ class CACTWP_setting {
                     array(
                         'id'    => 'copy_clipboard_selector',
                         'type'  => 'text',
-                        'title' => 'Selector',
+                        'title' => __( 'Selector', 'catcwp' ),
                         ),
                       
                       array(
                         'id'    => 'copy_clipboard_text',
                         'type'  => 'text',
-                        'title' => 'Text',
+                        'title' => __( 'Text', 'catcwp' ),
                       ),
                   
                       array(
                         'id'    => 'copy_clipboard_content',
                         'type'  => 'textarea',
-                        'title' => 'Content',
+                        'title' => __( 'Content', 'catcwp' ),
                       ),
                       array(
                         'id'      => 'copy_clipboard_typography',
                         'type'    => 'typography',
-                        'title'   => 'Typography',
-                        'output'  => 'span.cacwp_text',
-                        'output_important' => true,
+                        'title' => __( 'Typography', 'catcwp' ),
                         'font_family'      => true,
                         'font_weight'      => true,
                         'subset'           => true,
@@ -116,15 +114,100 @@ class CACTWP_setting {
                       array(
                         'id'          => 'copy_clipboard_bg_color',
                         'type'        => 'color',
-                        'title'       => 'Background Color',
+                        'title' => __( 'Background Color', 'catcwp' ),
                         'output_mode' => 'background-color',
-                        'default' => 'transparent'
+                        'default' => '#ffffff'
                       ),
                       array(
-                        'id'    => 'copy_clipboard_color',
+                        'id'          => 'copy_clipboard_bg_hover_color',
+                        'type'        => 'color',
+                        'title' => __( 'Background Hover Color', 'catcwp' ),
+                        'output_mode' => 'background-color',
+                        'default' => '#ffffff'
+                      ),
+                      array(
+                        'id'      => 'copy_clipboard_color',
                         'type'    => 'color',
-                        'title'   => 'Color',
+                        'title'   => __( 'Color', 'catcwp' ),
                         'default' => '#000000'
+                      ),
+                      array(
+                        'id'      => 'copy_clipboard_hover_color',
+                        'type'    => 'color',
+                        'title'   => __( 'Hover Color', 'catcwp' ),
+                        'default' => '#000000'
+                      ),
+                      array(
+                        'id'               => 'copy_clipboard_padding',
+                        'type'             => 'spacing',
+                        'title'            => __( 'Padding', 'catcwp' ),
+                        'output_mode'      => 'padding',
+                        'default'          => array(
+                            'top'    => '0',
+                            'right'  => '0',
+                            'bottom' => '0',
+                            'left'   => '0',
+                            'unit'   => 'px',
+                        ),
+                      ),
+                      array(
+                        'id'               => 'copy_clipboard_margin',
+                        'type'             => 'spacing',
+                        'title'            => __( 'Margin', 'catcwp' ),
+                        'output_mode'      => 'margin',
+                        'default'          => array(
+                            'top'    => '0',
+                            'right'  => '0',
+                            'bottom' => '0',
+                            'left'   => '0',
+                            'unit'   => 'px',
+                        ),
+                      ),
+                      array(
+                        'id'               => 'copy_clipboard_border',
+                        'type'             => 'border',
+                        'title'            => __( 'Border', 'catcwp' ),
+                        'default'          => array(
+                            'style'  => 'solid',
+                            'color'  => '#ffffff',
+                            'top'    => '0',
+                            'right'  => '0',
+                            'bottom' => '0',
+                            'left'   => '0',
+                            'unit'   => 'px',
+                        ),
+                    ),
+                    array(
+                        'type'    => 'heading',
+                        'content' => __( 'Border radius', 'catcwp' ),
+                    ),
+                      array(
+                          'id'      => 'copy_clipboard_border_radius_top',
+                          'type'    => 'number',
+                          'unit'    => 'px',
+                          'default' => '0',
+                          'title'   => __( 'Top', 'catcwp' ),
+                      ),
+                      array(
+                          'id'      => 'copy_clipboard_border_radius_right',
+                          'type'    => 'number',
+                          'unit'    => 'px',
+                          'default' => '0',
+                          'title'   => __( 'Right', 'catcwp' ),
+                      ),
+                      array(
+                          'id'      => 'copy_clipboard_border_radius_bottom',
+                          'type'    => 'number',
+                          'unit'    => 'px',
+                          'default' => '0',
+                          'title'   => __( 'Bottom', 'catcwp' ),
+                      ),
+                      array(
+                          'id'      => 'copy_clipboard_border_radius_left',
+                          'type'    => 'number',
+                          'unit'    => 'px',
+                          'default' => '0',
+                          'title'   => __( 'Left', 'catcwp' ),
                       ),
                     )
                   ),                  
--- includes/class-catcwp-shortcode.php
+++ includes/class-catcwp-shortcode.php
@@ -12,6 +12,7 @@ class CATCWP_Shortcode {
      * Shortcode Render Function
      */
     public function shortcode_generator($atts, $content = null) {
+
         ob_start();
         $settings = CACTWP_setting::get_settings();
         $copy_clipboard_lists = $settings['copy_clipboard_lists'];
@@ -19,8 +20,12 @@ class CATCWP_Shortcode {
         $copy_clipboard_selector = [];
         $copy_clipboard_text = [];
         $copy_clipboard_content = [];
+
         $copy_clipboard_bg_color = [];
+        $copy_clipboard_bg_hover_color = [];
         $copy_clipboard_color = [];
+        $copy_clipboard_hover_color = [];
+
         $copy_clipboard_font_family = [];
         $copy_clipboard_font_weight = [];
         $copy_clipboard_text_align = [];
@@ -29,13 +34,40 @@ class CATCWP_Shortcode {
         $copy_clipboard_line_height = [];
         $copy_clipboard_letter_spacing = [];
 
+        $copy_clipboard_padding_top = [];
+        $copy_clipboard_padding_right = [];
+        $copy_clipboard_padding_bottom = [];
+        $copy_clipboard_padding_left = [];
+
+        $copy_clipboard_margin_top = [];
+        $copy_clipboard_margin_right = [];
+        $copy_clipboard_margin_bottom = [];
+        $copy_clipboard_margin_left = [];
+
+        $copy_clipboard_border_top = [];
+        $copy_clipboard_border_right = [];
+        $copy_clipboard_border_bottom = [];
+        $copy_clipboard_border_left = [];
+        $copy_clipboard_border_color = [];
+        $copy_clipboard_border_style = [];
+
+        $copy_clipboard_border_radius_top = [];
+        $copy_clipboard_border_radius_right = [];
+        $copy_clipboard_border_radius_bottom = [];
+        $copy_clipboard_border_radius_left = [];
+
         if(!empty($copy_clipboard_lists)){
             foreach($copy_clipboard_lists as $item){
+
                 $copy_clipboard_selector[] = strtolower($item['copy_clipboard_selector']);
                 $copy_clipboard_text[] = $item['copy_clipboard_text'];
                 $copy_clipboard_content[] = $item['copy_clipboard_content'];
-                $copy_clipboard_color[] = $item['copy_clipboard_color'];
+
                 $copy_clipboard_bg_color[] = $item['copy_clipboard_bg_color'];
+                $copy_clipboard_bg_hover_color[] = $item['copy_clipboard_bg_hover_color'];
+                $copy_clipboard_color[] = $item['copy_clipboard_color'];
+                $copy_clipboard_hover_color[] = $item['copy_clipboard_hover_color'];
+
                 $copy_clipboard_font_family [] = $item['copy_clipboard_typography']['font-family'];
                 $copy_clipboard_font_weight [] = $item['copy_clipboard_typography']['font-weight'];
                 $copy_clipboard_text_align [] = $item['copy_clipboard_typography']['text-align'];
@@ -43,6 +75,28 @@ class CATCWP_Shortcode {
                 $copy_clipboard_font_size [] = $item['copy_clipboard_typography']['font-size'];
                 $copy_clipboard_line_height [] = $item['copy_clipboard_typography']['line-height'];
                 $copy_clipboard_letter_spacing [] = $item['copy_clipboard_typography']['letter-spacing'];
+
+                $copy_clipboard_padding_top [] = $item['copy_clipboard_padding']['top'];
+                $copy_clipboard_padding_right [] = $item['copy_clipboard_padding']['right'];
+                $copy_clipboard_padding_bottom [] = $item['copy_clipboard_padding']['bottom'];
+                $copy_clipboard_padding_left [] = $item['copy_clipboard_padding']['left'];
+
+                $copy_clipboard_margin_top [] = $item['copy_clipboard_margin']['top'];
+                $copy_clipboard_margin_right [] = $item['copy_clipboard_margin']['right'];
+                $copy_clipboard_margin_bottom [] = $item['copy_clipboard_margin']['bottom'];
+                $copy_clipboard_margin_left [] = $item['copy_clipboard_margin']['left'];
+
+                $copy_clipboard_border_top [] = $item['copy_clipboard_border']['top'];
+                $copy_clipboard_border_right [] = $item['copy_clipboard_border']['right'];
+                $copy_clipboard_border_bottom [] = $item['copy_clipboard_border']['bottom'];
+                $copy_clipboard_border_left [] = $item['copy_clipboard_border']['left'];
+                $copy_clipboard_border_color [] = $item['copy_clipboard_border']['color'];
+                $copy_clipboard_border_style [] = $item['copy_clipboard_border']['style'];
+
+                $copy_clipboard_border_radius_top [] = $item['copy_clipboard_border_radius_top'];
+                $copy_clipboard_border_radius_right [] = $item['copy_clipboard_border_radius_right'];
+                $copy_clipboard_border_radius_bottom [] = $item['copy_clipboard_border_radius_bottom'];
+                $copy_clipboard_border_radius_left [] = $item['copy_clipboard_border_radius_left'];
             }
         }
 
@@ -50,11 +104,16 @@ class CATCWP_Shortcode {
 
         // check if the 'tag' value matches a value in $copy_clipboard_selector array
         if (in_array($tag, $copy_clipboard_selector)) {
+
             $index = array_search($tag, $copy_clipboard_selector);
             $custom_text = $copy_clipboard_text[$index];
             $custom_content = $copy_clipboard_content[$index];
-            $custom_color = $copy_clipboard_color[$index];
+
             $custom_bg_color = $copy_clipboard_bg_color[$index];
+            $custom_bg_hover_color = $copy_clipboard_bg_hover_color[$index];
+            $custom_color = $copy_clipboard_color[$index];
+            $custom_hover_color = $copy_clipboard_hover_color[$index];
+
             $custom_font_family = $copy_clipboard_font_family[$index];
             $custom_font_weight = $copy_clipboard_font_weight[$index];
             $custom_text_align = $copy_clipboard_text_align[$index];
@@ -62,12 +121,38 @@ class CATCWP_Shortcode {
             $custom_font_size = $copy_clipboard_font_size[$index];
             $custom_line_height = $copy_clipboard_line_height[$index];
             $custom_letter_spacing = $copy_clipboard_letter_spacing[$index];
+
+            $custom_padding_top = $copy_clipboard_padding_top[$index];
+            $custom_padding_right = $copy_clipboard_padding_right[$index];
+            $custom_padding_bottom = $copy_clipboard_padding_bottom[$index];
+            $custom_padding_left = $copy_clipboard_padding_left[$index];
+
+            $custom_margin_top = $copy_clipboard_margin_top[$index];
+            $custom_margin_right = $copy_clipboard_margin_right[$index];
+            $custom_margin_bottom = $copy_clipboard_margin_bottom[$index];
+            $custom_margin_left = $copy_clipboard_margin_left[$index];
+
+            $custom_border_top = $copy_clipboard_border_top[$index];
+            $custom_border_right = $copy_clipboard_border_right[$index];
+            $custom_border_bottom = $copy_clipboard_border_bottom[$index];
+            $custom_border_left = $copy_clipboard_border_left[$index];
+            $custom_border_color = $copy_clipboard_border_color[$index];
+            $custom_border_style = $copy_clipboard_border_style[$index];
+
+            $custom_border_radius_top = $copy_clipboard_border_radius_top[$index];
+            $custom_border_radius_right = $copy_clipboard_border_radius_right[$index];
+            $custom_border_radius_bottom = $copy_clipboard_border_radius_bottom[$index];
+            $custom_border_radius_left = $copy_clipboard_border_radius_left[$index];
         } else {
             //set default value
             $custom_text = 'Click to copy';
             $custom_content = '';
-            $custom_bg_color = 'transparent';
-            $custom_color = 'black';
+
+            $custom_bg_color = '#ffffff';
+            $custom_bg_hover_color = '#ffffff';
+            $custom_color = '#000000';
+            $custom_hover_color = '#000000';
+
             $custom_font_family = 'inherit';
             $custom_font_weight = 'inherit';
             $custom_text_align = 'inherit';
@@ -76,10 +161,31 @@ class CATCWP_Shortcode {
             $custom_line_height = 'inherit';
             $custom_letter_spacing = 'inherit';
 
+            $custom_padding_top = '0';
+            $custom_padding_right = '0';
+            $custom_padding_bottom = '0';
+            $custom_padding_left = '0';
+
+            $custom_margin_top = '0';
+            $custom_margin_right = '0';
+            $custom_margin_bottom = '0';
+            $custom_margin_left = '0';
+
+            $custom_border_top = '0';
+            $custom_border_right = '0';
+            $custom_border_bottom = '0';
+            $custom_border_left = '0';
+            $custom_border_color = '#fff';
+            $custom_border_style = 'solid';
+
+            $custom_border_radius_top = '0';
+            $custom_border_radius_right = '0';
+            $custom_border_radius_bottom = '0';
+            $custom_border_radius_left = '0';
+
         }
 
-
-        // $custom_text = 'Click to copy';
+        // Content Display Condition
         if (empty($content)) {
             $text = isset($atts['text']) ? $atts['text'] : $custom_text;
             $content = isset($atts['content']) ? $atts['content'] : $custom_content;
@@ -89,8 +195,12 @@ class CATCWP_Shortcode {
         }
         
         ?>
+        
         <style>
             span.cacwp_text{
+                background-color: <?php echo $custom_bg_color; ?>;
+                color: <?php echo $custom_color; ?>;
+
                 font-family: <?php echo $custom_font_family; ?>;
                 font-weight: <?php echo $custom_font_weight; ?>;
                 text-align: <?php echo $custom_text_align; ?>;
@@ -98,10 +208,36 @@ class CATCWP_Shortcode {
                 font-size: <?php echo $custom_font_size; ?>px;
                 line-height: <?php echo $custom_line_height; ?>px;
                 letter-spacing: <?php echo $custom_letter_spacing; ?>px;
-                background-color: <?php echo $custom_bg_color; ?>;
-                color: <?php echo $custom_color; ?>;
+
                 cursor: pointer;
-                display: inline;
+                display: inline-block;
+                transition: ease-in-out .5s;
+
+                padding-top: <?php echo $custom_padding_top; ?>px;
+                padding-right: <?php echo $custom_padding_right; ?>px;
+                padding-bottom: <?php echo $custom_padding_bottom; ?>px;
+                padding-left: <?php echo $custom_padding_left; ?>px;
+
+                margin-top: <?php echo $custom_margin_top; ?>px;
+                margin-right: <?php echo $custom_margin_right; ?>px;
+                margin-bottom: <?php echo $custom_margin_bottom; ?>px;
+                margin-left: <?php echo $custom_margin_left; ?>px;
+
+                border-top: <?php echo $custom_border_top; ?>px;
+                border-right: <?php echo $custom_border_right; ?>px;
+                border-bottom: <?php echo $custom_border_bottom; ?>px;
+                border-left: <?php echo $custom_border_left; ?>px;
+                border-color: <?php echo $custom_border_color; ?>;
+                border-style: <?php echo $custom_border_style; ?>;
+
+                border-top-left-radius:     <?php echo esc_html($custom_border_radius_top); ?>px;
+                border-top-right-radius:    <?php echo esc_html($custom_border_radius_right); ?>px;
+                border-bottom-right-radius: <?php echo esc_html($custom_border_radius_bottom); ?>px;
+                border-bottom-left-radius:  <?php echo esc_html($custom_border_radius_left); ?>px;
+            }
+            span.cacwp_text:hover {
+                background-color: <?php echo $custom_bg_hover_color; ?>;
+                color: <?php echo $custom_hover_color; ?>;
             }
         </style>
 
