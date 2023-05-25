ac7c358250d30eb674555c89f82552c788a2489a
noruzzamanrubel <noruzzamanrubel@gmail.com>
2023-05-25 08:33:10 +0600

    update function name

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // Add a new column to the admin post list
-function add_custom_shortcode_column($columns) {
+function Catcwp_add_custom_shortcode_column($columns) {
     $new_columns = array();
     foreach ($columns as $key => $value) {
         $new_columns[$key] = $value;
@@ -10,10 +10,10 @@ function add_custom_shortcode_column($columns) {
     }
     return $new_columns;
 }
-add_filter('manage_copy_to_clipboard_posts_columns', 'add_custom_shortcode_column');
+add_filter('manage_copy_to_clipboard_posts_columns', 'Catcwp_add_custom_shortcode_column');
 
 // Populate the custom column with data
-function display_custom_shortcode_column($column, $post_id) {
+function Catcwp_display_custom_shortcode_column($column, $post_id) {
     if ($column === 'shortcode') {
         // Output the shortcode with dynamic post ID
         $meta = get_post_meta( get_the_ID(), 'copy_to_clipboard_options', true );
@@ -21,10 +21,10 @@ function display_custom_shortcode_column($column, $post_id) {
         echo '[copy_clipboard tag=' . $copy_clipboard_selector . ']';
     }
 }
-add_action('manage_copy_to_clipboard_posts_custom_column', 'display_custom_shortcode_column', 10, 2);
+add_action('manage_copy_to_clipboard_posts_custom_column', 'Catcwp_display_custom_shortcode_column', 10, 2);
 
 // Adjust column order
-function move_shortcode_column($columns) {
+function Catcwp_move_shortcode_column($columns) {
     $shortcode = $columns['shortcode'];
     unset($columns['shortcode']);
     $new_columns = array();
@@ -36,11 +36,11 @@ function move_shortcode_column($columns) {
     }
     return $new_columns;
 }
-add_filter('manage_copy_to_clipboard_posts_columns', 'move_shortcode_column');
+add_filter('manage_copy_to_clipboard_posts_columns', 'Catcwp_move_shortcode_column');
 
 // Make the column sortable
-function make_shortcode_column_sortable($columns) {
+function Catcwp_make_shortcode_column_sortable($columns) {
     $columns['shortcode'] = 'shortcode';
     return $columns;
 }
-add_filter('manage_edit-copy_to_clipboard_sortable_columns', 'make_shortcode_column_sortable');
+add_filter('manage_edit-copy_to_clipboard_sortable_columns', 'Catcwp_make_shortcode_column_sortable');
